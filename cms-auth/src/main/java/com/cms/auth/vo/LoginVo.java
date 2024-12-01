e444c240c3c5c64433122d469d74e2722169f4a5
可汗开源地 <12698596+yawu668@user.noreply.gitee.com>
2024-12-01 14:53:36 +0800

    init

--- cms-auth/src/main/java/com/cms/auth/vo/LoginVo.java
+++ cms-auth/src/main/java/com/cms/auth/vo/LoginVo.java
@@ -22,9 +22,11 @@ public class LoginVo {
     @NotBlank(message = "用户名不能为空")
     private String username;
 
+
     /**
      * 用户密码
      */
     @NotBlank(message = "用户密码不能为空")
+
     private String password;
 }
