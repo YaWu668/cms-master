08dca72a1a2ae69a4286ab5c96846fb47ff84e76
可汗开源地 <yawwu668@foxmail.comgit config --global user.email yawwu668@foxmail.com>
2025-04-19 01:12:12 +0800

    refactor(cms-models):优化富文本字段的错误提示信息
    - 在 ApplyForA 类中，为所有的 @RichText 注解添加了自定义错误提示信息- 新的错误提示信息更加具体，能够准确地指出格式不正确的字段
    - 此修改提高了用户在输入不符合规范的内容时的友好提示，提升了用户体验

--- cms-models/cms-models-xiangmu/src/main/java/com/xk/domain/dto/ApplyForA.java
+++ cms-models/cms-models-xiangmu/src/main/java/com/xk/domain/dto/ApplyForA.java
@@ -14,31 +14,31 @@ public class ApplyForA {
      * 七.2: 已具备的条件，尚缺少的条件及解决方法
      */
     @JsonProperty("aEight")
-    @RichText
+    @RichText(message = "七.2: 已具备的条件，尚缺少的条件及解决方法格式不正确")
     private String aEight;
     /**
      * 五.技术路线、拟解决的问题及预期成果
      */
     @JsonProperty("aFive")
-    @RichText
+    @RichText(message = "五.技术路线、拟解决的问题及预期成果格式不正确")
     private String aFive;
     /**
      * 四.创新点与项目特色
      */
     @JsonProperty("aFour")
-    @RichText
+    @RichText(message = "四.创新点与项目特色格式不正确")
     private String aFour;
     /**
      * 一.研究目的
      */
     @JsonProperty("aOne")
-    @RichText
+    @RichText(message = "一.研究目的格式不正确")
     private String aOne;
     /**
      * 七.1: 与本项目有关的研究积累和已取得的成绩
      */
     @JsonProperty("aSeven")
-    @RichText
+    @RichText(message = "七.1: 与本项目有关的研究积累和已取得的成绩格式不正确")
     private String aSeven;
     /**
      * 六.项目研究进度安排
@@ -50,12 +50,12 @@ public class ApplyForA {
      * 三.国、内外研究现状和发展动态
      */
     @JsonProperty("aThree")
-    @RichText
+    @RichText(message = "三.国、内外研究现状和发展动态格式不正确")
     private String aThree;
     /**
      * 二.研究内容
      */
     @JsonProperty("aTwo")
-    @RichText
+    @RichText(message = "二.研究内容格式不正确")
     private String aTwo;
 }
