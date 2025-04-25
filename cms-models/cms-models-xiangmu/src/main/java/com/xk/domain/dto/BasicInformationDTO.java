7439ba2256c66279b26e9cb6591023b4a589919f
可汗开源地 <yawwu668@foxmail.comgit config --global user.email yawwu668@foxmail.com>
2025-04-25 23:18:34 +0800

    fix(xiangmu): 修正项目名称格式验证正则表达式- 将原有的 Pattern 注解正则表达式从 "[\\\\\\\\/:*?\\\"<>|]" - 修改为 "^[^\\\\/:*?\"<>|]+$" - 新正则表达式确保项目名称不包含 \ / : * ? "< > | 等禁止字符

--- cms-models/cms-models-xiangmu/src/main/java/com/xk/domain/dto/BasicInformationDTO.java
+++ cms-models/cms-models-xiangmu/src/main/java/com/xk/domain/dto/BasicInformationDTO.java
@@ -43,7 +43,7 @@ public class BasicInformationDTO {
     @NotNull(message = "项目名称不能为空")
     @NotEmpty(message = "项目名称不能为空")
     @RichText
-    @Pattern(regexp = "[\\\\\\\\/:*?\\\"<>|]" ,message = "项目名称格式不正确,下禁止的字符: \\ / : * ? \" < > |")
+    @Pattern( regexp = "^[^\\\\/:*?\"<>|]+$",message = "项目名称格式不正确,下禁止的字符: \\ / : * ? \" < > |")
     private String name;
     /**
      * 负责人曾经参与科研的情况
