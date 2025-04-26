5a08a113142cde228ed43e4ea3c2eb1588edcfae
script <2271277962@qq.com>
2025-04-26 19:31:42 +0800

    -修复 用户列表接口未正确返回角色id列表

--- cms-models/cms-models-system/src/main/java/com/cms/system/controller/SysUserController.java
+++ cms-models/cms-models-system/src/main/java/com/cms/system/controller/SysUserController.java
@@ -138,6 +138,7 @@ public class SysUserController extends BaseController {
         this.startPage();
         // 2.查询数据
         List<SysUserDto> users = this.userService.listEntities(query);
+        users.forEach(user -> user.setRoleIds(userService.getUserRolesIds(user.getUserId())));
         // 3.返回数据
         return this.getDataTable(users);
     }
